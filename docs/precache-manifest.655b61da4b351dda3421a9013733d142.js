4f74bf98432354ff842d8d64a975ea09d700b622
Javier Perea <javierlperea@gmail.com>
2020-07-17 08:32:42 -0300

    docs agregado

---
+++ docs/precache-manifest.655b61da4b351dda3421a9013733d142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ad37fa31198f49a9f4589856fa3b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1dca196cf8fc413c444",
+    "url": "/static/css/main.95dd1730.chunk.css"
+  },
+  {
+    "revision": "45bf1f440661b90ee646",
+    "url": "/static/js/2.ff7b0c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff7b0c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1dca196cf8fc413c444",
+    "url": "/static/js/main.8ed24dc3.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
